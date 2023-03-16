223e38f91d739f30d7b1d487b3d3125c591fbe7a
IgorR <igorrozhnov95@gmail.com>
2023-03-17 00:15:23 +0300

    added functions

--- project/__init__.py
+++ project/__init__.py
@@ -9,8 +9,8 @@ from flask_migrate import Migrate
 app=Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@db/test'
-
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@db/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@127.0.0.1:5433/test'
 
 db=SQLAlchemy(app)
 migrate=Migrate(app, db)
@@ -19,4 +19,4 @@ migrate=Migrate(app, db)
 
 
 
-# from project import views
+from project import models
--- project/models.py
+++ project/models.py
@@ -2,10 +2,10 @@ from project import db
 
 class Accrual(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
-    date = db.Column(db.DateTime())
+    date = db.Column(db.Date())
     month = db.Column(db.Integer())
 
 class Payment(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
-    date = db.Column(db.DateTime())
+    date = db.Column(db.Date())
     month = db.Column(db.Integer())    
