26bf58c90589956ac54e0838e634e056404b9a65
IgorR <igorrozhnov95@gmail.com>
2023-03-17 09:27:38 +0300

    added docstrings and type annotations

--- project/functions.py
+++ project/functions.py
@@ -1,20 +1,56 @@
 from . import db
+from flask import request
 from project.models import Accrual, Payment
+from typing import Any
 
 
 
-def days_between_dates(date1, date2):
+def get_request_args_for_task_1() -> dict[str, Any]:
+    """
+    Определяет аргументы запроса и возвращает их в словаре.
+    """
+    date1 = request.args.get('date1')
+    date2 = request.args.get('date2')
+                      
+    return {'date1': date1,'date2': date2}
+
+
+
+def get_request_args_for_task_2() -> dict[str, Any]:
+    """
+    Определяет аргументы запроса и возвращает их в словаре.
+    """
+    num = request.args.get('num')
+    k = request.args.get('k')
+    
+    if k[0] == '-' or num[0] == '-':
+        raise Exception('\'k\' must be accepted')
+                                  
+    return {'num': num,'k': int(k)}
+    
+   
+   
+def days_between_dates(date1: str, date2: str) -> dict[str, int]:
+    """
+    Определяет количество дней в каждой из 2-х дат аргументов, начиная с 1 января года 0
+    и возвращает словарь с разницей между количеством дней в датах.
+    """
     year1, month1, day1 = map(int, date1.split('-'))
     year2, month2, day2 = map(int, date2.split('-'))
     
-    # количество дней в каждой из дат, начиная с 1 января года 0
     days1 = day1 + 365 * year1 + (year1 // 4) - (year1 // 100) + (year1 // 400) + ((month1 * 306 + 5) // 10) - 307
     days2 = day2 + 365 * year2 + (year2 // 4) - (year2 // 100) + (year2 // 400) + ((month2 * 306 + 5) // 10) - 307
     
-    # разница между количеством дней в двух датах
-    return abs(days2 - days1)
+    return {'Number of days between dates': abs(days2 - days1)}
+
+
 
 def remove_k_digits(num: str, k: int) -> str:
+    """
+    Принимает целое положительное число в виде строки и целое число k 
+    и возвращает словарь с минимальным возможным числом строкой, 
+    полученным после удаления из строки k цифр. 
+    """
     stack = []
     for digit in num:
         while stack and k and digit < stack[-1]:
@@ -24,41 +60,40 @@ def remove_k_digits(num: str, k: int) -> str:
     while k > 0:
         stack.pop()
         k -= 1
-    return ''.join(stack).lstrip('0') or '0'
+    
+    return {'The minimum number after removing \'k\' digits from the string \'num\'': ''.join(stack).lstrip('0') or '0'}
 
 
-def get_accrual_list():
+
+def get_accrual_list() -> list[tuple]:
+    """
+    Получает из БД и возвращает список кортежей долгов.
+    """
     accrual_list = db.session.query(Accrual.id, Accrual.date, Accrual.month).order_by(Accrual.month).all()
+    
     return accrual_list
 
 
-def get_payment_list():
+
+def get_payment_list() -> list[tuple]:
+    """
+    Получает из БД и возвращает список кортежей платежей.
+    """
     payment_list = db.session.query(Payment.id, Payment.date, Payment.month).order_by(Payment.month).all()
+    
     return payment_list
 
-def get_payment_table_and_remaining_payment_list():
-    """
-    функция приводит в соответствие долги и платежи, принимает курсор БД sqlite3
-    обращается к таблицам платежей и долгов и приводит их в соответствие в 2 этапа:
-    сперва в соответствие долгам приводятся платежи совпадающие с ними по месяцу, и имеющие более позднюю дату
-    затем остальные, имеющие более позднюю дату в порядке от самых старых долгов
-    :param cursor:
-    :return: таблица распределенных платежей в виде списка, а также список не нашедших соответствия платежей
-    """
 
-    # при выборке из БД упорядочиваем по возрастанию даты платежей и долгов
-    # sql = "SELECT * FROM accrual ORDER BY month, date"
-    # cursor.execute(sql)
+
+def get_payment_table_and_remaining_payment_list() -> tuple[dict[tuple, tuple], list[tuple]]:
+    """
+    Функция приводит в соответствие долги и платежи, совпадающие по месяцу.
+    Возвращает кортеж со словарем соответствий и списком с кортежами оставшихся платежей.
+    """
     accrual_list = get_accrual_list()
-
-    # sql = "SELECT * FROM payment ORDER BY month, date"
-    # cursor.execute(sql)
     payment_list = get_payment_list()
-
-    # создаем словарь с ключами в виде долгов, которым в соответствие будем приводить id платежей.
     payment_table = {accrual: None for accrual in accrual_list}
 
-    # проверка и присваивание первого этапа, более приоритетная
     for accrual in payment_table:
         if not payment_table[accrual]:
             for payment in payment_list:
@@ -69,14 +104,18 @@ def get_payment_table_and_remaining_payment_list():
     return payment_table, payment_list                
 
 
-def get_list_of_not_used_payments_and_payment_table():
+
+def get_list_of_not_used_payments_and_payment_table()  -> dict[str, Any]:
+    """
+    Функция приводит в соответствие долги и платежи, несовпадающие по месяцу,
+    и создает отдельный список неиспользованных платежей.
+    Возвращает словарь со списком словарей "долг-платёж" и списком платежей, которые не нашли себе долг.
+    """
     payment_table_and_remaining_payment_list = get_payment_table_and_remaining_payment_list()
     payment_table = payment_table_and_remaining_payment_list[0]
     payment_list = payment_table_and_remaining_payment_list[1]
-    # здесь будут храниться платежи, которые не нашли себе долга
     list_of_not_used_payments = []
 
-    # проверка и присваивание второго этапа, оставшиеся платежи
     searching = True
     while searching:
         if payment_list:
@@ -94,16 +133,17 @@ def get_list_of_not_used_payments_and_payment_table():
         else:
             searching = False
         if all(payment_table.values()):
-            # Добавляем в список оставшиеся платежи
             list_of_not_used_payments.extend(payment_list)
             searching = False
             
-    return list_of_not_used_payments, payment_table    
-
-    # Создадим список с кортежами из долгов и платежей.
-def get_list():
-    list_of_not_used_payments_and_payment_table = get_list_of_not_used_payments_and_payment_table()
-    payment_table = [(accrual, payment) for accrual, payment in list_of_not_used_payments_and_payment_table[1].items()]
-    return payment_table
-    # return payment_table, list_of_not_used_payments
-
+    return {
+        'Accrual, Payment': 
+        [[{'id': accrual[0], 'date': str(accrual[1]), 'month': accrual[2]},
+          {'id': payment[0], 'date': str(payment[1]), 'month': payment[2]}] 
+         for accrual,payment in list(payment_table.items())],
+    
+    'List of not used payments': 
+        [{'id': id, 'date': str(date), 'month': month} for id,date,month in list_of_not_used_payments]
+        }
+    
+    
